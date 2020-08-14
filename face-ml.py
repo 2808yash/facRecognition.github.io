b85dd10031fec8def19eba9222d17b3f71d56426
Yash Sharma <67325907+2808yash@users.noreply.github.com>
2020-08-14 15:42:16 +0530

    Update face-ml.py

--- face-ml.py
+++ face-ml.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 st.title("Face Recognition using OpenCV")
 img=st.file_uploader("Upload any Image here for faces detection", type=("png", "jpg","jpeg"))
-facecascade=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
+facecascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 if img is not None:
     imag = Image.open(img)
     st.header("Original Image : ")
